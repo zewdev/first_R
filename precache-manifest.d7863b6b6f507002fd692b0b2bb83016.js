457cfc0de033c6a95c9c4f1d6a3cea434b0a14a9
zewdev <41309660+zewdev@users.noreply.github.com>
2019-08-15 19:25:09 +0100

    Updates

---
+++ precache-manifest.d7863b6b6f507002fd692b0b2bb83016.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86e877efe45fa44a20401b3549fab5fe",
+    "url": "/first_R/index.html"
+  },
+  {
+    "revision": "cd2252958d057acb3762",
+    "url": "/first_R/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2edf9ce3aa2fd34c20b9",
+    "url": "/first_R/static/js/2.f7f8f703.chunk.js"
+  },
+  {
+    "revision": "cd2252958d057acb3762",
+    "url": "/first_R/static/js/main.6ce9ab28.chunk.js"
+  },
+  {
+    "revision": "a0a8fcddac333238d303",
+    "url": "/first_R/static/js/runtime~main.a509de26.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/first_R/static/media/logo.5d5d9eef.svg"
+  }
+]);
